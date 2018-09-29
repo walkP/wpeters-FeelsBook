9a6580bdcda7fd7afd56b9894094af8c67e22b11
walkP <wpeters@ualberta.ca>
2018-09-29 16:41:04 -0600

    Storyboard added

--- app/src/main/java/com/example/walker/feelsbook/MainActivity.java
+++ app/src/main/java/com/example/walker/feelsbook/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.walker.feelsbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
